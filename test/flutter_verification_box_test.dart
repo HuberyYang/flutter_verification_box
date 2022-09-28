75ada8c5d102153ac3b5e6a8b25550c97a458e1f
minliang <minliang.yang@jimuitech.com>
2022-09-28 10:42:13 +0800

    Compatible with 3.0.3

--- test/flutter_verification_box_test.dart
+++ test/flutter_verification_box_test.dart
@@ -1,5 +1,7 @@
+// ignore: unused_import
 import 'package:flutter_test/flutter_test.dart';
 
+// ignore: unused_import
 import 'package:flutter_verification_box/src/verification_box.dart';
 
 void main() {
